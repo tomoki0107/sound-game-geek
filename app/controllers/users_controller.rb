1662d77640beb42764fb80d40a0c7af36bdcf683
tomoki0107 <tttttnozawa0340@gmail.com>
2020-12-07 15:23:52 +0900

    詳細ページの表示とeditアクションの定義

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,8 +4,13 @@ class UsersController < ApplicationController
   end
 
   def show
+    @user = User.find(params[:id])
   end
 
   def edit
+    @user = User.find(params[:id])
   end
+
+  private
+
 end
