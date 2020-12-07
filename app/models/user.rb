c09ac2581a0d09aad55ff72318bbc311dc8a4dc3
tomoki0107 <tttttnozawa0340@gmail.com>
2020-12-07 15:08:36 +0900

    投稿者一覧画面の作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,5 +7,6 @@ class User < ApplicationRecord
   
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
   validates_format_of :password, with: PASSWORD_REGEX, message: 'Include both letters and numbers'
+  attachment :profile_image
 end
 
