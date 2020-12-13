0021097ef1d1ae9b2dec2b64c99341c55dd893e9
tomoki0107 <tttttnozawa0340@gmail.com>
2020-12-13 14:02:35 +0900

    edit,show,updateアクションの実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,18 @@ class UsersController < ApplicationController
   def edit
     @user = User.find(params[:id])
   end
+  
+  def update
+    if current_user.update(user_params)
+      redirect_to user_path
+    else
+      render :edit
+    end
+  end
 
   private
 
+  def user_params
+    params.require(:user).permit(:nickname, :email, :profile, :profile_image)
+  end
 end
