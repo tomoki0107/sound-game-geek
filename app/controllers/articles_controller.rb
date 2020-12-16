3c294bad46ac76be28c7ab718bc4e649be6f4579
tomoki0107 <tttttnozawa0340@gmail.com>
2020-12-16 18:33:32 +0900

    createアクションの実装

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,9 +6,21 @@ class ArticlesController < ApplicationController
     @article = Article.new
   end
 
+  def create
+    @article = Article.new(article_params)
+    @article.save
+    redirect_to article_path(@article)
+  end
+
   def show
   end
 
   def edit
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:image, :title, :info, :category_id).merge(user_id: current_user.id)
   end
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -5,5 +5,5 @@ class Article < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
 
-  validates :genre_id, numericality: { other_than: 1 }
+  validates :category_id, numericality: { other_than: 1 }
 end
