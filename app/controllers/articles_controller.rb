4051fcc2215dbd4a08515f924d060c084d29e7e9
tomoki0107 <tttttnozawa0340@gmail.com>
2020-12-17 12:34:03 +0900

    destroyアクションの定義

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -27,6 +27,12 @@ class ArticlesController < ApplicationController
     redirect_to article_path(@article)
   end
 
+  def destroy
+    article = Article.find(params[:id])
+    article_params.destroy
+    redirect_to articles_path
+  end
+
   private
 
   def article_params
