1201c296bb374e57cd50905390318d416ccc0580
tomoki0107 <tttttnozawa0340@gmail.com>
2020-12-18 23:55:30 +0900

    バリデーションの設定

--- app/models/article.rb
+++ app/models/article.rb
@@ -5,5 +5,10 @@ class Article < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
 
+  with_options presence: true do
+    validates :title
+    validates :image
+    validates :info
+  end
   validates :category_id, numericality: { other_than: 1 }
 end
